22797e484422e55b22ea58769c78ff21ba942c42
Mario Zahariev <webbersof@gmail.com>
2021-06-07 11:47:26 +0300

    first commit

---
+++ Petstagram/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+import Petstagram
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('Petstagram.common.urls')),
+]
